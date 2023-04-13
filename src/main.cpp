35cae4b1dd53402370988dad10a448975b89d63c
Faisal Alghamdi <faisal@punch.rip>
2023-04-13 19:46:24 +0300

    Remove uneeded code

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-
-#if QT_VERSION >= 0x50601
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
     if (engine.rootObjects().isEmpty())
         return -1;
 
